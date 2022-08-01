ec3506bfedea3da5c3f939661cc3de4f127a6c4e
IMCH005 <1628225498@qq.com>
2022-08-01 19:56:19 +0800

    feat: TodoGroup.jsx TodoGroup.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import TodoGenerator from './components/TodoGenerator';
 import TodoItem from './components/TodoItem';
+import TodoGroup from './components/TodoGroup';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <TodoItem />
+      <TodoGroup />
     </div>
   );
 }
---
+++ src/components/TodoGroup.jsx
@@ -0,0 +1,13 @@
+import TodoItem from "./TodoItem";
+
+function TodoGroup() {
+  return (
+    <div className="todo-group">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  );
+}
+
+export default TodoGroup;
